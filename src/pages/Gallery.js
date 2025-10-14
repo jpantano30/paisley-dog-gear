4929fbf84ed7d8f22121173b867d14c8baa4ed43
Jena <jpantano30@gmail.com>
2025-10-14 15:49:40 -0400

    added to gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,24 +8,27 @@ const galleryImages = [
   { filename: "painted.JPG", caption: "Custom Painted Collar", type: "image" },
   { filename: "navy_brown.JPG", caption: "Deep Sea and Brown Standard Leash", type: "image" },
   { filename: "/hands-free-coral_Navy.JPG", caption: "Hands-free system in coral and deep sea with brass hardware", type: "image" },
-
   { filename: "tallulah-sage.JPG", caption: "The Tallulah in Sage & White", type: "image" },
   { filename: "handsfree-redandblack2.JPG", caption: "Hands-free standard red and black", type: "image" },
   { filename: "Turbo_gym.JPEG", caption: "Custom Collar with Name Plate", type: "image" },
+  { filename: "navy&brown8ft.JPG", caption: "Navy & Brown 8ft Leash", type: "image" },
+  { filename: "coralnsagehandsfree.JPG", caption: "Hands-free system in coral and sage with brass hardware", type: "image" },
+  { filename: "ball&leash.JPG", caption: "Ball Holder and Leash Set", type: "image" },
   { filename: "/leashpicsNvids/IMG_5191.JPG", caption: "Custom 2 Tone Collar", type: "image" },
-
+  { filename: "IMG_8753.JPG", caption: "Custom 2 Tone Collar", type: "image" },
   { filename: "JetTug.jpeg", caption: "Jet showing how durable his red and black leash is!", type: "image" },
 
   { filename: "PurpleTrafficNCollar_ocean.jpg", caption: "Amythest Purple traffic handle/ pull tab and collar.", type: "image" },
   { filename: "/leashpicsNvids/rwb.jpg", caption: "Hands-free 8ft red, white and blue (3-tone) leash and matching adjustable collar", type: "image" },
-
+  { filename: "IMG_84080.JPG", caption: "Custom 2 Tone Collar", type: "image" },
   { filename: "rolled_coral.JPG", caption: "The Tallulah in Coral & Navy", type: "image" },
 
   { filename: "littleone1.jpeg", caption: "Little One in her matching collar and leash set", type: "image" },
-
+  { filename: "IMG_8763.JPG", caption: "Custom 2 Tone Collar", type: "image" },
   { filename: "collars_custom.JPG", caption: "Custom Collars", type: "image" },
   { filename: "leashextender.jpg", caption: "Leash extender (sage & white with black hardware)", type: "image" },
   { filename: "/leashpicsNvids/bella.jpg", caption: "Custom hands-free leash in amethyst purple and violet", type: "image" },
+  { filename: "tullycircle.JPG", caption: "Tully in her custom collar", type: "image" },
   { filename: "Turbo_painted.jpg", caption: "Turbo rocking a hand painted collar!", type: "image" },
   { filename: "/leashpicsNvids/Pan.jpg", caption: "Custom hands-free leash in blue 522 and deep sea", type: "image" },
   { filename: "custom-longline.jpg", caption: "Custom Leash in Violet", type: "image" },
